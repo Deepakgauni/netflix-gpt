02eabdc07efea2c7d45abbfebded6c74e3f8a12c
Deepakgauni <161502912+Deepakgauni@users.noreply.github.com>
2025-09-24 01:03:01 +0530

    adding logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 
 import './index.css'
-
+import Body from './components/Body'
 function App() {
 
 
   return (
     <>
-       <h1 class="text-3xl font-bold underline flex justify-center">
-    Hello world!
-  </h1>
+     <Body />
     </>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+      login
+      <Header />
+    </div>
+  )
+}
+
+export default Login
