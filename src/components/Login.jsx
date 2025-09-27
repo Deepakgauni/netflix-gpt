cc221301a742aec5d82965920261ba980de42d67
Deepakgauni <161502912+Deepakgauni@users.noreply.github.com>
2025-09-28 00:59:02 +0530

    Password Authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState,useRef } from 'react'
 import Header from './Header'
 import {checkValidData} from '../utils/validate'
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../utils/firebase'
 
 const Login = () => {
   const[isSignInForm, setIsSignInForm]= useState(true);
@@ -10,8 +12,47 @@ const Login = () => {
    const password= useRef(null);
 
   const handleButtonClick= ()=>{
-     const message= checkValidData(email.current.value,password.current.value);
+     const message= checkValidData(email.current.value, password.current.value);
      setErrorMessage(message);
+     if(message)return;
+
+     // sign in or sign up logic
+     if(!isSignInForm){
+        // sign up logic
+     createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    console.log(user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+     setErrorMessage(errorCode + "-" + errorMessage);
+    // ..
+  });
+
+
+     }
+
+     else{
+        // sign in logic
+        signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(errorCode + "-" + errorMessage)
+  });
+     }
+
+
   }
 
   const toogleSignInform=()=>{
@@ -21,7 +62,8 @@ const Login = () => {
     <div>
       <Header />
       <div className='absolute'>
-        <img src='https://assets.nflxext.com/ffe/siteui/vlv3/bebd95d0-65f9-41a9-9d12-4794db63653e/web/IN-en-20250922-TRIFECTA-perspective_5e75cfb4-3797-4f17-866b-181ff91a51dd_large.jpg' />
+        <img src='https://assets.nflxext.com/ffe/siteui/vlv3/bebd95d0-65f9-41a9-9d12-4794db63653e/web/IN-en-20250922-TRIFECTA-perspective_5e75cfb4-3797-4f17-866b-181ff91a51dd_large.jpg' 
+       alt="Netflix background" />
       </div>
       <form   onSubmit={(e) => e.preventDefault()} className='w-3/12 absolute p-12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-black/70'>
       <h1 className='text-3xl font-bold my-4'>
