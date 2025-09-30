12f379f0c7602a548d106b8b1e64ca79cec87af2
Deepakgauni <161502912+Deepakgauni@users.noreply.github.com>
2025-10-01 00:52:03 +0530

    update profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { useSelector } from 'react-redux'
 
 const Header = () => {
   const navigate = useNavigate();
-  const user = useSelector(store=>store)
+  const user = useSelector(store=>store.user)
 const handleSignOut=()=>{
   
 signOut(auth).then(() => {
@@ -23,10 +23,10 @@ signOut(auth).then(() => {
         <img className='w-44 '
           src='https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='netfflx logo' />
       
-      <div  className='flex '>
-      <img className='w-10 h-10' src='https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.webp' />
+     { user && <div  className='flex '>
+      <img className='w-10 h-10' src={user?.photoURL} />
       <button onClick={handleSignOut}>(sign Out)</button>
-      </div>
+      </div>}
       </div>
     </>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,13 +10,14 @@ import {addUser} from '../utils/userSlice'
 
 const Login = () => {
   const[isSignInForm, setIsSignInForm]= useState(true);
-  const [errorMessage, setErrorMessage]= useState("");
+  const [errorMessage, setErrorMessage]= useState(null);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+ const name= useRef(null)
   const email= useRef(null);
    const password= useRef(null);
-   const name= useRef(null)
+  
 
   const handleButtonClick= ()=>{
      const message= checkValidData(email.current.value, password.current.value);
@@ -31,20 +32,19 @@ const Login = () => {
 
     const user = userCredential.user;
     updateProfile(user, {
-  displayName: name.current.value, 
-  photoURL: ""
-}).then(() => {
-     const {uid, email, displayName, photoURL}= auth.currentUser;
-      dispatch(addUser({uid:uid, email:email, displayName:displayName, photoURL:photoURL}));
-  navigate("/browse")
+    // displayName: name.current.value, 
+    // photoURL: "https://avatars.githubusercontent.com/u/161502912?v=4"
+})
+.then(() => {
+    const {uid, email, displayName, photoURL}= auth.currentUser;
+    dispatch(addUser({uid:uid, email:email, displayName:displayName, photoURL:photoURL}));
+      navigate("/browse")
   // ...
 }).catch((error) => {
 setErrorMessage(error.message);
   // ...
 });
-  
 
-    // ...
   })
   .catch((error) => {
     const errorCode = error.code;
