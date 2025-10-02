018be7fa3d40f60ca9152a111deafb5cc2ae0af2
Deepakgauni <161502912+Deepakgauni@users.noreply.github.com>
2025-10-03 01:02:53 +0530

    get data from tmdb and add into redux store

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,2 +1,11 @@
 export const LOGO= "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const USER_AVATAR = "https://wallpapers.com/images/high/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.webp";
+
+
+export const API_OPTIONS= {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YWE2NDgwM2UzZjAzOTdkN2M3NTNlZDhlNzA2NGZhOCIsIm5iZiI6MTc1OTM0NjY2Ny43ODQ5OTk4LCJzdWIiOiI2OGRkN2ZlYjkwOTU3YTM5OWZiOWIxYjkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.1Ju82lardf3HjwiyfcUYYgidqeKRxouk1mrfK2Ie3TE'
+  }
+};
