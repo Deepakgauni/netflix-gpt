44983557594676b9698f6d27dc5e2dd824211e44
Deepakgauni <161502912+Deepakgauni@users.noreply.github.com>
2025-11-16 00:20:59 +0530

    add a video background and create a addtrailervideo slice

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -10,12 +10,13 @@ const MainContainer = () => {
     const mainMovie = movies[0];
     console.log(mainMovie);
 
-    const {original_title, overview} = mainMovie;
+    const {original_title, overview, id } = mainMovie;
 
   return (
     <div>
-      <VideoBackground />
-      <VideoTitle title= {original_title} overview ={overview} />
+    <VideoTitle title= {original_title} overview ={overview} />
+      <VideoBackground movieId={id} />
+    
     </div>
   )
 }
