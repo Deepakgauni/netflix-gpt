22951ec48a3acd0253f6bc9baafce6271e0bcd0b
Deepakgauni <161502912+Deepakgauni@users.noreply.github.com>
2025-12-20 00:28:50 +0530

    create a gptsearch  button

---
+++ src/components/GptSearchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const GptSearchBar = () => {
+  return (
+    <div className='pt-[20%] flex justify-center'>
+      <form className=' w-1/2 bg-black grid grid-cols-12' >
+      <input type='text' placeholder='What would you like to watch today' className='p-4 m-4 bg-white col-span-9' />
+      <button className=' col-span-3 py-2 px-4 m-4 bg-red-700 text-white rounded-lg'>Search</button>
+
+      </form>
+    </div>
+  )
+}
+
+export default GptSearchBar
